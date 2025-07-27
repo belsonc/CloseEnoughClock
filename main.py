361c32fd67d544dea1dc8f601f1b60dab0efdb02
belsonc <craig.belson@gmail.com>
2025-07-27 18:34:22 -0400

    C: initial code base is complete, spot checking and looking to see what else I need to adjust

--- main.py
+++ main.py
@@ -2,37 +2,34 @@ import pygame
 import sys
 from pygame.locals import *
 import time
-import datetime
+from datetime import datetime
 
 
 
 def main():
     pygame.init()
-    font = pygame.font.SysFont("Arial", 36)
-    displaysurf = pygame.display.set_mode((800,600))
+    font = pygame.font.SysFont("Arial", 96)
+    displaysurface = pygame.display.set_mode((800,600))
     pygame.display.set_caption('Eh, it\'s about...')
 
     minute_color = (2, 38, 129) #my birthday :-)
     descriptor_color =  (9, 22, 119) #my wife's birthday :-)
-    hour_color = (5, 20, 34) #our anniversary :-) :-)
+    hour_color = (16, 6, 109) #mathematical color progression
 
     minute_modifier_list = ["just", "five", "ten", "quarter", "twenty", "twenty five", "half"]
     descriptor_modifier_list = ["about", "after", "past", "to"]
-    while True:
+    running = True
+    while running:
         time_of_day = datetime.now()
         current_time = time_of_day.strftime("%H:%M:%S")
         current_hour = int(time_of_day.strftime("%H"))
         current_minute = int(time_of_day.strftime("%M"))
-        match current_minute: #58, 59, 0, 1, 2
-                                #3 - 27
-                                #28 - 58
+        match current_minute:
             case 58 | 59 | 0 | 1 | 2:
-                print("something")
                 descriptor_modifier = descriptor_modifier_list[0] #about
-                minute_modifier = "something"
+                minute_modifier = minute_modifier_list[0] #just
                 #insert code as necessary to adjust the hour value
             case num if 3 <= num <= 27: 
-                print("something")
                 descriptor_modifier = descriptor_modifier_list[1] #after
                 if 3 <= num <= 7:
                     minute_modifier = minute_modifier_list[1] #5
@@ -46,7 +43,6 @@ def main():
                     minute_modifier = minute_modifier_list[5] #25
                 #insert code as necessary to adjust the hour value
             case num if 28 <= num < 58:
-                print("something")
                 descriptor_modifier = descriptor_modifier_list[3] #to
                 if 28 <= num <= 32:
                     descriptor_modifier = descriptor_modifier_list[2] #past - this is the only case where past will be used
@@ -88,65 +84,67 @@ def main():
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 2 | 14:
                 hour_text = "two"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 3 | 15:
                 hour_text = "three"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 4 | 16:
                 hour_text = "four"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 5 | 17:
                 hour_text = "five"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 6 | 18:
                 hour_text = "six"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 7 | 19:
                 hour_text = "seven"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 8 | 20:
                 hour_text = "eight"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 9 | 21:
                 hour_text = "nine"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 10 | 22:
                 hour_text = "ten"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
             case 11 | 23:
                 hour_text = "eleven"
-                print("something else")
             case 12 | 0:
                 if current_hour == 12:
                     hour_text = "noon"
                 else:
                     hour_text = "midnight"
-                print("something else")
                 #insert code to correct for if it's pm or am, if it's close to the next hour, etc
                 #insert code as necessary to adjust the hour value
 
-        #put code here to draw the text to the screen
-        #three calls to font.render, three calls to text.surface.get_rect
-        #screen.fill
-        #screen.blit (3x)
         
+        minute_img = font.render(minute_modifier, True, minute_color)
+        
+        hour_img = font.render(hour_text, True, hour_color)
+        
+        descriptor_img = font.render(descriptor_modifier, True, descriptor_color)
+
+        minute_rect = minute_img.get_rect()
+        hour_rect = hour_img.get_rect()
+        descriptor_rect = descriptor_img.get_rect()
+
+
+
         for event in pygame.event.get():
             if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+                running = False
+                return
+        
+        displaysurface.fill("black")
+        displaysurface.blit(minute_img, (20, 20))
+        displaysurface.blit(descriptor_img, (20, 220))
+        displaysurface.blit(hour_img, (20, 420))
         pygame.display.update()
-        time.sleep(60)
 
-
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
